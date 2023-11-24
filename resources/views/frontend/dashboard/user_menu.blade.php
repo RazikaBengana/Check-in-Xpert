6cc7714a6e6e5e55cad07a8ae2c367d06bf1da52
RazikaBengana <razikabengana@gmail.com>
2023-11-24 20:19:58 +0100

    Update user profile form: add `jQuery` (for DOM --> "Document Object Model" manipulation, and for event handling), and add form controls for name, email, address, phone, and image upload

--- resources/views/frontend/dashboard/user_menu.blade.php
+++ resources/views/frontend/dashboard/user_menu.blade.php
@@ -1,9 +1,18 @@
+@php
+    $id = Auth::user()->id;
+    $profiledata = App\Models\User::find($id);
+@endphp
+
 <div class="service-side-bar">
     <div class="services-bar-widget">
         <h3 class="title">User Sidebar</h3>
         <div class="side-bar-categories">
-            <img src="{{ asset('frontend/assets/img/blog/blog-profile1.jpg') }}" class="rounded mx-auto d-block" alt="Image" style="width:100px; height:100px;"> <br><br>
-
+            <img src="{{ (!empty($profileData->photo)) ? url('upload/user_images/'.$profileData->photo) : url('upload/no_image.jpg')}}" class="rounded mx-auto d-block" alt="Image" style="width:100px; height:100px;">
+            <center>
+            <b>{{ $profiledata->name }}</b><br>
+                <b>{{ $profiledata->email }}</b>
+            </center>
+            <br>
             <ul>
 
                 <li>
